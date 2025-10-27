873f174bb41b9b7a87d25056209edbf06e9737e9
Taisiia <taya13taya@gmail.com>
2025-10-27 22:49:43 +0200

    not-found page

---
+++ src/components/LegalNotice/NotFound.tsx
@@ -0,0 +1,24 @@
+import styles from './NotFound.module.css'
+import img from "../../../public/img/404.png"
+import commonStyles from '../common.module.css';
+import { useNavigate } from 'react-router-dom';
+
+export default function NotFound() {
+    const navigate = useNavigate();
+
+    const openMain = () => {
+		navigate('/');
+	};
+
+	return (
+		<div className={styles.notfound}>
+            <div className={styles.notfound0}>
+                <div className={styles.notfound_photo}>
+                    <img src ={img}></img>
+                    <p>This page has gone fishing...</p>
+                </div>
+                <button className={`${commonStyles.nextStepButton} ${styles.mainPage}`} onClick={openMain}>Return to main page</button>
+            </div>
+		</div>
+	)
+}
